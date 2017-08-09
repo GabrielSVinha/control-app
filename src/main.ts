8177871f525498034d070b4883566f65128cf9eb
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-08-09 20:17:50 -0300

    Add bootstraping

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
+import { firebaseConfig } from './config/firebaseConfig';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import * as firebase from 'firebase';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
-
 platformBrowserDynamic().bootstrapModule(AppModule);
