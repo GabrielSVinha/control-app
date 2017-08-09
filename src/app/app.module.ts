82f4873fb3f8d15295b9519bdce0b050b761a8b5
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-08-09 20:18:48 -0300

    Last few alterations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { LoginService } from './login.service';
+import { firebaseConfig } from './../config/firebaseConfig';
 import { MaterialModule } from '@angular/material';
 import { TaxiService } from './taxi.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import * as firebase from 'firebase';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +12,11 @@ import { TaxiCardComponent } from './taxi-card/taxi-card.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ROUTING } from '../config/routes';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuardDirective } from './auth-guard.directive';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
@@ -17,14 +24,21 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     NavbarComponent,
     HomeComponent,
     TaxiCardComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
+    FormsModule,
     RouterModule.forRoot(ROUTING)
   ],
-  providers: [TaxiService],
+  providers: [
+    TaxiService,
+    AuthGuardDirective,
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
