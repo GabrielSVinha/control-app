75e5be571d08dbf244f7d46e3ff05ea676fc7352
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-08-09 20:18:21 -0300

    Update routing redirection

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,10 @@
 import { PageNotFoundComponent } from './../app/page-not-found/page-not-found.component';
 import { HomeComponent } from './../app/home/home.component';
 import { Routes } from '@angular/router';
-
+import { AuthGuardDirective } from '../app/auth-guard.directive';
+import { LoginComponent } from '../app/login/login.component';
 export const ROUTING: Routes = [
-    {path: '', component: HomeComponent},
+    { path: '', component: HomeComponent, canActivate: [AuthGuardDirective] },
+    { path: 'login', component: LoginComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
