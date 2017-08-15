9409d2fc91816affa3c127729d534bda3c08ebf1
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-08-15 16:28:44 -0300

    Change to angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { MaterialModule } from '@angular/material';
 import { TaxiService } from './taxi.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import * as firebase from 'firebase';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -15,8 +14,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthGuardDirective } from './auth-guard.directive';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
-firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
@@ -31,7 +32,10 @@ firebase.initializeApp(firebaseConfig);
     BrowserModule,
     MaterialModule,
     FormsModule,
-    RouterModule.forRoot(ROUTING)
+    RouterModule.forRoot(ROUTING),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [
     TaxiService,
